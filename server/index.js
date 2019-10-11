6175ff1726ea0c24ba9686860ff5e3757bfa2207
Sean Pollock <seanpollock3344@gmail.com>
2019-10-11 19:21:18 -0400

    Working locally

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const crypto = require('crypto')
 const util = require('util')
 const writeFile = util.promisify(require('fs').writeFile)
 const mkdir = util.promisify(require('fs').mkdir)
+const readFile = util.promisify(require('fs').readFile)
 const { exec } = require('child_process')
-
 
 const app = express()
 const upload = multer()
@@ -14,13 +14,22 @@ app.post('/compile', upload.single('file'), async (req, res, next) => {
   const id = crypto.randomBytes(16).toString('hex')
   console.log('### New file upload. id:', id)
   const fileBuffer = Buffer.from(req.body.file, 'hex')
-  const { body: { fileName }} = req
+  const fileBuffer2 = Buffer.from(req.body.file2, 'hex')
+  const { body: { fileName, fileName2 }} = req
   await mkdir(id)
   await writeFile(`${id}/${fileName}`, fileBuffer)
-  exec(`contract_path=${process.cwd()}/${id}/${fileName} id=${id} contract_name=${fileName} . ./compile.sh`, (err, stdout, stderr) => {
+  await writeFile(`${id}/${fileName2}`, fileBuffer2)
+  exec(`contract_path=${process.cwd()}/${id}/${fileName} id=${id} contract_name=${fileName} . ./compile.sh`, async (err, stdout, stderr) => {
     console.log(err)
     console.log(stderr)
     console.log(stdout)
+
+    const wasmFile = await readFile(`${id}/contract_name.out.wasm`)
+    const jsFile = await readFile(`${id}/contract_name.out.js`)
+    res.send(JSON.stringify({
+      wasmFile,
+      jsFile
+    }))
   })
 
 })
--- server/test.js
+++ server/test.js
@@ -7,16 +7,19 @@ const port = 7050
 
 const test = (async () => {
   const cppFile = await readFile('tmp/FungibleToken.cpp')
+  const hFile = await readFile('tmp/FungibleToken.h')
   const form = new FormData()
   form.append('file', cppFile.toString('hex'))
   form.append('fileName', 'FungibleToken.cpp')
+  form.append('file2', hFile.toString('hex'))
+  form.append('fileName2', 'FungibleToken.h')
   const response = await axios({
     method: 'post',
-    url: `http://localhost:${port}/compile`,
+    url: `http://35.203.71.42:${port}/compile`,
     data: form,
     headers: {
       'content-type': `multipart/form-data; boundary=${form._boundary}`
     }
   })
-
+  console.log("response", response)
 })()
