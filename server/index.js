24692a63e05fb96ddd14c2439af5cc03c8e1888c
Sean Pollock <seanpollock3344@gmail.com>
2019-10-12 18:17:49 -0400

    fixing error handling. moving code into smaller functions

--- server/index.js
+++ server/index.js
@@ -12,38 +12,72 @@ const upload = multer()
 
 app.post('/compile', upload.single('file'), async (req, res, next) => {
   try {
-    const id = crypto.randomBytes(16).toString('hex')
-    console.log('### New file upload. id:', id)
-    const fileBuffer = Buffer.from(req.body.file, 'hex')
-    const fileBuffer2 = Buffer.from(req.body.file2, 'hex')
-    const { body: { fileName, fileName2 }} = req
-    await mkdir(id)
-    await writeFile(`${id}/${fileName}`, fileBuffer)
-    await writeFile(`${id}/${fileName2}`, fileBuffer2)
-  }
-  catch (error) {
+    const { id, fileName, fileBuffer, fileName2, fileBuffer2 } = parse(req)
+    console.log('### Uploaded new code. id:', id)
+    await write(id, fileName, fileBuffer, fileName2, fileBuffer2)
+    await emcc(id, fileName)
+    console.log('after emcc')
+    const response = await read(id)
+    res.send(response)
+    await clean(id)
+    console.log('### Deleted compiled code. id:', id)
+  } catch (error) {
     next(error)
   }
+})
 
+/* UTILS */
 
-  exec(`contract_path=${process.cwd()}/${id}/${fileName} id=${id} contract_name=${fileName} . ./compile.sh`, async (err, stdout, stderr) => {
-    if (err) next(err)
-    if (stderr) next(stderr)
+const parse = (req) => {
+  const id = crypto.randomBytes(16).toString('hex')
+  const fileBuffer = Buffer.from(req.body.file, 'hex')
+  const fileBuffer2 = Buffer.from(req.body.file2, 'hex')
+  const { body: { fileName, fileName2 }} = req
+  return {
+    id,
+    fileName,
+    fileBuffer,
+    fileName2,
+    fileBuffer2
+  }
+}
+
+const write = async (id, fileName, fileBuffer, fileName2, fileBuffer2) => {
+  await mkdir(id)
+  await writeFile(`${id}/${fileName}`, fileBuffer)
+  await writeFile(`${id}/${fileName2}`, fileBuffer2)
+}
+
+const emcc = (id, fileName) => new Promise((resolve, reject) => {
+  exec(`contract_path=${process.cwd()}/${id}/${fileName} id=${id} contract_name=${fileName} . ./compile.sh`,
+  (err, stdout, stderr) => {
+    console.log('inside emcc')
+    if (err) reject(err)
+    if (stderr) console.log(stderr)
     console.log(stdout)
-
-    try {
-      const wasmFile = await readFile(`${id}/contract_name.out.wasm`)
-      const jsFile = await readFile(`${id}/contract_name.out.js`)
-      res.send(JSON.stringify({
-        wasmFile: wasmFile.toString('hex'),
-        jsFile: jsFile.toString('hex')
-      }))
-    }
-    catch (error) {
-      next(error)
-    }
+    console.log('about to resolve')
+    resolve(true)
   })
+})
 
+const read = async (id) => {
+  console.log('inside read')
+  const wasmFile = await readFile(`${id}/contract_name.out.wasm`)
+  const jsFile = await readFile(`${id}/contract_name.out.js`)
+  return JSON.stringify({
+    wasmFile: wasmFile.toString('hex'),
+    jsFile: jsFile.toString('hex')
+  })
+}
+
+const clean = (id) => new Promise((resolve, reject) => {
+  exec(`rm -rf ${process.cwd()}/${id}`,
+  (err, stdout, stderr) => {
+    if (err) reject(err)
+    if (stderr) console.log(stderr)
+    console.log(stdout)
+    resolve(stdout)
+  })
 })
 
 
