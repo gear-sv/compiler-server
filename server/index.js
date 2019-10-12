2f1372c37afb2dd4ddf080afecc607822df5108f
Sean Pollock <seanpollock3344@gmail.com>
2019-10-12 17:22:17 -0400

    adding express error handling

--- server/index.js
+++ server/index.js
@@ -11,25 +11,37 @@ const app = express()
 const upload = multer()
 
 app.post('/compile', upload.single('file'), async (req, res, next) => {
-  const id = crypto.randomBytes(16).toString('hex')
-  console.log('### New file upload. id:', id)
-  const fileBuffer = Buffer.from(req.body.file, 'hex')
-  const fileBuffer2 = Buffer.from(req.body.file2, 'hex')
-  const { body: { fileName, fileName2 }} = req
-  await mkdir(id)
-  await writeFile(`${id}/${fileName}`, fileBuffer)
-  await writeFile(`${id}/${fileName2}`, fileBuffer2)
+  try {
+    const id = crypto.randomBytes(16).toString('hex')
+    console.log('### New file upload. id:', id)
+    const fileBuffer = Buffer.from(req.body.file, 'hex')
+    const fileBuffer2 = Buffer.from(req.body.file2, 'hex')
+    const { body: { fileName, fileName2 }} = req
+    await mkdir(id)
+    await writeFile(`${id}/${fileName}`, fileBuffer)
+    await writeFile(`${id}/${fileName2}`, fileBuffer2)
+  }
+  catch (error) {
+    next(error)
+  }
+
+
   exec(`contract_path=${process.cwd()}/${id}/${fileName} id=${id} contract_name=${fileName} . ./compile.sh`, async (err, stdout, stderr) => {
-    console.log(err)
-    console.log(stderr)
+    if (err) next(err)
+    if (stderr) next(stderr)
     console.log(stdout)
 
-    const wasmFile = await readFile(`${id}/contract_name.out.wasm`)
-    const jsFile = await readFile(`${id}/contract_name.out.js`)
-    res.send(JSON.stringify({
-      wasmFile: wasmFile.toString('hex'),
-      jsFile: jsFile.toString('hex')
-    }))
+    try {
+      const wasmFile = await readFile(`${id}/contract_name.out.wasm`)
+      const jsFile = await readFile(`${id}/contract_name.out.js`)
+      res.send(JSON.stringify({
+        wasmFile: wasmFile.toString('hex'),
+        jsFile: jsFile.toString('hex')
+      }))
+    }
+    catch (error) {
+      next(error)
+    }
   })
 
 })
