d2b397b331dd9d947f4fcf3e289ff2f359190d5e
Sean Pollock <seanpollock3344@gmail.com>
2019-10-27 15:22:42 -0400

    healthz route, --profiler

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.post('/emcc', upload.single('file'), async (req, res, next) => {
 
 /* HEALTH CHECKS */
 
-app.get('/', (req, res, next) => {
+app.get('/healthz', (req, res, next) => {
   res.json({status: 'UP'})
 })
 
